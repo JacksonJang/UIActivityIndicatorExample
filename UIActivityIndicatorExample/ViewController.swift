dbb9dac71c41c16fc04e86427eba08bc3d1ebf7a
장효원 <hyo961015@naver.com>
2021-12-24 18:04:48 +0900

    Simple Example

--- UIActivityIndicatorExample/ViewController.swift
+++ UIActivityIndicatorExample/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        activityIndicator.style = .large
+        
+        //Default startAnimating
+        activityIndicator.startAnimating()
     }
 
-
+    @IBAction func onTouchStartButton(_ sender: Any) {
+        activityIndicator.startAnimating()
+    }
+    
+    @IBAction func onTouchStopButton(_ sender: Any) {
+        activityIndicator.stopAnimating()
+    }
+    
 }
 
